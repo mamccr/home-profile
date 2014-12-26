a1ac39f296440dec9e853f95d472f405a2ac978f
Mark McCraw <mmccraw@tendrilinc.com>
2014-12-26 13:05:50 -0700

    tying ember-data to Energize API

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('homeProfiles', function() {
+    this.resource('homeProfile');
+  });
 });
 
 export default Router;
