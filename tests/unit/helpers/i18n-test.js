0dcafaf69ab5254b2a1566664fc0e6a6cd3b9521
Mark McCraw <mmccraw@tendrilinc.com>
2014-12-29 13:43:39 -0700

    Wrapping up connection to Rails back-end.  Displaying information in the most rudimentary form possible.  Next:  localization.

---
+++ tests/unit/helpers/i18n-test.js
@@ -0,0 +1,11 @@
+import {
+  i18n
+} from 'home-profile-ember/helpers/i18n';
+
+module('I18nHelper');
+
+// Replace this with your real tests.
+test('it works', function() {
+  var result = i18n(42);
+  ok(result);
+});
